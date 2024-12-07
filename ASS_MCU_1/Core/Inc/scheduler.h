2b470bdaf71f6071443d778deb9739ca7012f37a
LAPTOP-L4QLQTHA\Hy <ngxuanhy1405@gmail.com>
2024-12-07 13:05:41 +0700

    update ass

--- ASS_MCU_1/Core/Inc/scheduler.h
+++ ASS_MCU_1/Core/Inc/scheduler.h
@@ -11,24 +11,21 @@
 #include "stdint.h"
 #include "main.h"
 
-#define SCH_MAX_TASK 40
+#define SCH_MAX_TASKS 	10
+#define	NO_TASK_ID		0
 
-typedef struct{
-	void (*pTask)(void);
-	uint32_t 	Delay;
-	uint32_t	Period;
-	uint8_t 	RunMe;
+typedef struct {
+	void ( * pTask)(void);
+	uint32_t Delay;
+	uint32_t Period;
+	uint8_t RunMe;
+	uint32_t TaskID;
+} sTask;
 
-	uint32_t	TaskID;
-}sTask;
-
-void SCH_Init(void);
-
-void SCH_Add_Task( void(*pFunction)() , uint32_t DELAY , uint32_t PERIOD);
-void SCH_Update(void);
-
-void SCH_Dispatch_Tasks(void);
-
-void SCH_Delete(uint32_t);
+void SCH_Init();
+void SCH_Update();
+void SCH_Add_Task(void (*p_function)(), uint32_t, uint32_t);
+void SCH_Dispatch_Tasks();
+void SCH_Delete_Task(uint32_t);
 
 #endif /* INC_SCHEDULER_H_ */
